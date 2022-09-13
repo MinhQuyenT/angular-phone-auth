94ad44a08b44ffbc699cf562e08bab77b6dabb49
(SSB-61B9) Quyen Tran <quyen.tran@ssbshoes.com>
2022-09-13 16:22:01 +0700

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAo0dsgNge7W02mn5qISi3L1FvAYiM8BMM",
+    authDomain: "phone-auth-f92fb.firebaseapp.com",
+    projectId: "phone-auth-f92fb",
+    storageBucket: "phone-auth-f92fb.appspot.com",
+    messagingSenderId: "780807476413",
+    appId: "1:780807476413:web:fbc7bb10aa9423d16653d4"
+  },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAo0dsgNge7W02mn5qISi3L1FvAYiM8BMM",
+    authDomain: "phone-auth-f92fb.firebaseapp.com",
+    projectId: "phone-auth-f92fb",
+    storageBucket: "phone-auth-f92fb.appspot.com",
+    messagingSenderId: "780807476413",
+    appId: "1:780807476413:web:fbc7bb10aa9423d16653d4"
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
